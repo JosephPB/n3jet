be8a545c5f7f01371627658d1cc566671f12464d
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-05-29 13:16:17 +0100

    adding nnet, and test weights

--- c++_calls/tests/single_test_infer.py
+++ c++_calls/tests/single_test_infer.py
@@ -41,28 +41,46 @@ test_momenta = test_momenta.tolist()
 nlegs = len(test_momenta[0])-2
 
 NN = Model(nlegs*4,test_momenta,test_nj,all_jets=True, all_legs=False)
-_,_,_,_,_,_,_,_ = NN.process_training_data()
+X_train,X_test,y_train,y_test,_,_,_,_ = NN.process_training_data()
 
-mod_dir = '/scratch/jbullock/Sherpa_NJet/runs/diphoton/3g2A/RAMBO/events_100k_single_0/'
+lr=0.01
 
-model = load_model(mod_dir + 'model')
+model, x_mean, x_std, y_mean, y_std = NN.fit(layers=[20,40,20], lr=lr, epochs=1)
 
-pickle_out = open(mod_dir + "/dataset_metadata.pickle","rb")
-metadata = pickle.load(pickle_out)
-pickle_out.close()
+mod_dir = '/scratch/jbullock/Sherpa_NJet/runs/diphoton/3g2A/RAMBO/'
+
+#model = load_model(mod_dir + 'model')
+
+#pickle_out = open(mod_dir + "/dataset_metadata.pickle","rb")
+#metadata = pickle.load(pickle_out)
+#pickle_out.close()
     
-x_mean = metadata['x_mean']
-y_mean = metadata['y_mean']
-x_std = metadata['x_std']
-y_std = metadata['y_std']
+#x_mean = metadata['x_mean']
+#y_mean = metadata['y_mean']
+#x_std = metadata['x_std']
+#y_std = metadata['y_std']
 
 x_standard = NN.process_testing_data(moms=test_momenta,x_mean=x_mean,x_std=x_std,y_mean=y_mean,y_std=y_std)
 
 testing_X = x_standard[:2]
 
+print (np.array(testing_X).shape)
+
 pred = model.predict(testing_X)
 
 print (pred)
 
+with open ('./single_test_arch.json', 'w') as fout:
+    fout.write(model.to_json())
+model.save_weights(mod_dir + 'single_test_weights.h5')
+
+print ('Model JSON and weights save')
+
+print ('Saving out sample data')
+
+with open('./single_test_sample.dat', 'w') as fin:
+    fin.write("1 1 {}\n".format(len(testing_X[0])))
+    fin.write(str(testing_X[0])+'\n')
+    
 
 
