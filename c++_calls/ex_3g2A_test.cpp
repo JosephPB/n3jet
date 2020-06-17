a13a11619ee1b3907be1caf92945d95b49274442
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-06-17 11:40:57 +0100

    seg fault after successfull compile

--- c++_calls/ex_3g2A_test.cpp
+++ c++_calls/ex_3g2A_test.cpp
@@ -46,13 +46,12 @@ int main()
     //nn::KerasModel kerasModel;
     //kerasModel.load_weights(dumpednn);
 
+    std::vector<nn::KerasModel> kerasModels;
 
-    std::vector<nn::KerasModel> kerasModels[2];
-    kerasModels[0] = KerasModel(dumpednn);
-    kerasModels[1] = KerasModel(dumpednn);
-    
-
-    
+    for (int j { 0 }; j < 2; ++j){
+      kerasModels.push_back(nn::KerasModel(dumpednn));
+    }
+        
     for (int i { 0 }; i < pspoints; ++i) {
         std::cout << "==================== Test point " << i + 1 << " ====================" << '\n';
 
@@ -75,7 +74,7 @@ int main()
         std::cout << '\n';
 #endif
 
-        std::vector<double> result { kerasModel.compute_output(mom) };
+        std::vector<double> result { kerasModels[0].compute_output(mom) };
 
         // destandardise output
         double output { nn::destandardise(result[0], y_mean, y_std) };
