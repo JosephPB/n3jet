b72e2dc9cc196e50b955475238cf4b425bcbb3c5
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2021-01-08 11:38:03 +0000

    initial adding of fks_utils tests

---
+++ tests/test_fks_utils.py
@@ -0,0 +1,10 @@
+from n3jet.utils.fks_utils import(
+    train_near_networks,
+    train_near_networks_general,
+    train_cut_network,
+    train_cut_network_general,
+    infer_on_near_splits,
+    infer_on_near_splits_separate,
+    infer_on_cut
+)
+
