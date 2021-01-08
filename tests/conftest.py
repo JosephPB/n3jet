b6322df48573a90a2b35a66fe907be87889c59ad
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2021-01-08 11:34:40 +0000

    putting dummy data in conftest

---
+++ tests/conftest.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pytest
+
+@pytest.fixture(name="dummy_data", scope="session")
+def make_dummy_data():
+
+    delta_cut = 0.01
+    delta_near = 0.02
+
+    cut_mom = [
+        np.array([250.,   0.,   0., 250.]),
+        np.array([ 250.,    0.,    0., -250.]),
+        np.array([ 159.22451821,   -6.63307095,  -84.13071446, -135.02026682]),
+        np.array([ 120.32231578, -109.77040355,  -49.14656498,    3.54024433]),
+        np.array([220.45316601, 116.4034745 , 133.27727944, 131.48002249])
+    ]
+
+    near_mom = [
+        np.array([250.,   0.,   0., 250.]),
+        np.array([ 250.,    0.,    0., -250.]),
+        np.array([216.40580479, -70.68745046, -56.43043907, 196.5969538 ]),
+        np.array([ 246.15021728,   86.55738039,   85.56729242, -213.95323749]),
+        np.array([ 37.44397793, -15.86992993, -29.13685335,  17.3562837 ])
+    ]
+    
+    momenta = []
+    momenta.append(cut_mom)
+    momenta.append(near_mom)
+
+    near_momenta = [near_mom]
+    cut_momenta = [cut_mom]
+
+    cut_label = 1.
+    near_label = 2.
+    
+    labels = []
+    labels.append(cut_label)
+    labels.append(near_label)
+
+    return momenta, cut_momenta, near_momenta, labels, cut_label, near_label, delta_cut, delta_near
+
+@pytest.fixture(name="dummy_data_all_legs", scope="session")
+def make_dummy_data_all_legs():
+
+    delta_cut = 0.01
+    delta_near = 0.02
+
+    cut_mom = [
+        np.array([250.,   0.,   0., 250.]),
+        np.array([ 250.,    0.,    0., -250.]),
+        np.array([ 159.22451821,   -6.63307095,  -84.13071446, -135.02026682]),
+        np.array([ 120.32231578, -109.77040355,  -49.14656498,    3.54024433]),
+        np.array([220.45316601, 116.4034745 , 133.27727944, 131.48002249])
+    ]
+
+    near_mom_all_legs =[
+        np.array([250.,   0.,   0., 250.]),
+        np.array([ 250.,    0.,    0., -250.]),
+        np.array([114.02383821,  29.90221969,  92.19870798, -60.05573399]),
+        np.array([191.46252681, -15.42504935,  59.08235119, 181.46416396]),
+        np.array([ 194.51363498,  -14.47717034, -151.28105917, -121.40842998])
+    ]
+
+    momenta_all_legs = []
+    momenta_all_legs.append(cut_mom)
+    momenta_all_legs.append(near_mom_all_legs)
+
+    near_momenta_all_legs = [near_mom_all_legs]
+    cut_momenta = [cut_mom]
+
+    cut_label = 1.
+    near_label = 2.
+    
+    labels = []
+    labels.append(cut_label)
+    labels.append(near_label)
+
+    return momenta_all_legs, cut_momenta, near_momenta_all_legs, labels, cut_label, near_label, delta_cut, delta_near
--- tests/test_fks_partition.py
+++ tests/test_fks_partition.py
@@ -2,48 +2,6 @@ import numpy as np
 
 from n3jet.utils import FKSPartition
 
-delta_cut = 0.01
-delta_near = 0.02
-
-cut_mom = [
-    np.array([250.,   0.,   0., 250.]),
-    np.array([ 250.,    0.,    0., -250.]),
-    np.array([ 159.22451821,   -6.63307095,  -84.13071446, -135.02026682]),
-    np.array([ 120.32231578, -109.77040355,  -49.14656498,    3.54024433]),
-    np.array([220.45316601, 116.4034745 , 133.27727944, 131.48002249])
-]
-
-near_mom = [
-    np.array([250.,   0.,   0., 250.]),
-    np.array([ 250.,    0.,    0., -250.]),
-    np.array([216.40580479, -70.68745046, -56.43043907, 196.5969538 ]),
-    np.array([ 246.15021728,   86.55738039,   85.56729242, -213.95323749]),
-    np.array([ 37.44397793, -15.86992993, -29.13685335,  17.3562837 ])
-]
-
-near_mom_all_legs =[
-    np.array([250.,   0.,   0., 250.]),
-    np.array([ 250.,    0.,    0., -250.]),
-    np.array([114.02383821,  29.90221969,  92.19870798, -60.05573399]),
-    np.array([191.46252681, -15.42504935,  59.08235119, 181.46416396]),
-    np.array([ 194.51363498,  -14.47717034, -151.28105917, -121.40842998])
-]
-
-cut_label = 1.
-near_label = 2.
-
-
-momenta = []
-momenta.append(cut_mom)
-momenta.append(near_mom)
-
-momenta_all_legs = []
-momenta_all_legs.append(cut_mom)
-momenta_all_legs.append(near_mom_all_legs)
-
-labels = []
-labels.append(cut_label)
-labels.append(near_label)
 
 def check_list(l1, l2):
     for idx, i in enumerate(l1):
@@ -51,8 +9,10 @@ def check_list(l1, l2):
             return False
     return True
 
-def test__cut_near_split():
+def test__cut_near_split(dummy_data):
 
+    momenta, cut_mom, near_mom, labels, cut_label, near_label, delta_cut, delta_near = dummy_data
+    
     fks = FKSPartition(
         momenta = momenta,
         labels = labels,
@@ -61,13 +21,15 @@ def test__cut_near_split():
 
     cut_momenta, near_momenta, cut_labels, near_labels = fks.cut_near_split(delta_cut, delta_near)
 
-    assert check_list(cut_momenta[0][2],cut_mom[2])
-    assert check_list(near_momenta[0][2], near_mom[2])
+    assert check_list(cut_momenta[0][2],cut_mom[0][2])
+    assert check_list(near_momenta[0][2], near_mom[0][2])
     assert cut_labels[0] == cut_label
     assert near_labels[0] == near_label
 
-def test__cut_near_split_all_legs():
+def test__cut_near_split_all_legs(dummy_data_all_legs):
 
+    momenta_all_legs, cut_mom, near_mom_all_legs, labels, cut_label, near_label, delta_cut, delta_near = dummy_data_all_legs
+    
     fks = FKSPartition(
         momenta = momenta_all_legs,
         labels = labels,
@@ -76,12 +38,14 @@ def test__cut_near_split_all_legs():
 
     cut_momenta, near_momenta, cut_labels, near_labels = fks.cut_near_split(delta_cut, delta_near)
 
-    assert check_list(cut_momenta[0][2],cut_mom[2])
-    assert check_list(near_momenta[0][2], near_mom_all_legs[2])
+    assert check_list(cut_momenta[0][2],cut_mom[0][2])
+    assert check_list(near_momenta[0][2], near_mom_all_legs[0][2])
     assert cut_labels[0] == cut_label
     assert near_labels[0] == near_label
 
-def test__weighting():
+def test__weighting(dummy_data):
+
+    momenta, cut_mom, near_mom, labels, cut_label, near_label, delta_cut, delta_near = dummy_data
     
     fks = FKSPartition(
            momenta = momenta,
@@ -97,7 +61,9 @@ def test__weighting():
     assert np.isclose(np.sum(np.array(labs_split)[:,0]),cut_label)
     assert np.isclose(np.sum(np.array(labs_split)[:,1]),near_label)
 
-def test__weighting_all_legs():
+def test__weighting_all_legs(dummy_data_all_legs):
+
+    momenta_all_legs, cut_mom, near_mom_all_legs, labels, cut_label, near_label, delta_cut, delta_near = dummy_data_all_legs
     
     fks = FKSPartition(
            momenta = momenta_all_legs,
