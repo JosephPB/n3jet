a0786a3e852c4076e3089f2bdfd3c0c37b56757e
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2021-01-13 12:31:44 +0000

    updating c++ single_test_train

--- n3jet/c++_calls/tests/single_test_train.py
+++ n3jet/c++_calls/tests/single_test_train.py
@@ -1,34 +1,17 @@
-import sys
-sys.path.append('./../')
-sys.path.append('./../../utils/')
-sys.path.append('./../../phase/')
-sys.path.append('./../../models/')
 import os
-
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-from matplotlib import cm
+from matplotlib import cm, rc
 import random
-from matplotlib import rc
 import time
 import cPickle as pickle
-import multiprocessing
 import argparse
-
-import rambo_while
-from njet_run_functions import *
-from model import Model
-from fks_partition import *
-from keras.models import load_model
 from tqdm import tqdm
-from fks_utils import *
-#from piecewise_utils import *
-from utils import *
-from uniform_utils import *
-import rambo_piecewise_balance as rpb
-from rambo_piecewise_balance import *
+from keras.models import load_model
+
+from n3jet.models import Model
 
 parser = argparse.ArgumentParser(description='Script for training a network and saving out the data in a .dat file')
 
@@ -45,7 +28,14 @@ test_momenta = test_momenta.tolist()
 
 nlegs = len(test_momenta[0])-2
 
-NN = Model((nlegs+2)*4,test_momenta,test_nj,all_jets=False, all_legs=True)
+NN = Model(
+    input_size = (nlegs+2)*4,
+    momenta = test_momenta,
+    labels = test_nj,
+    all_jets = False,
+    all_legs = True
+)
+
 X_train,X_test,y_train,y_test,_,_,_,_ = NN.process_training_data()
 
 lr=0.01
@@ -63,15 +53,26 @@ metadata = pickle.dump(metadata, pickle_out)
 pickle_out.close()
 
 
-x_standard = NN.process_testing_data(moms=test_momenta,x_mean=x_mean,x_std=x_std,y_mean=y_mean,y_std=y_std)
+x_standard = NN.process_testing_data(
+    moms=test_momenta,
+    x_mean=x_mean,
+    x_std=x_std,
+    y_mean=y_mean,
+    y_std=y_std
+)
 
-test_momenta = [[500., 0., 0., 500.],[500., 0., 0., -500.],[253.58419798, -239.58965912, 66.81985738, -49.36443422],
-                [373.92489886, 7.43568582, -321.18384469, 191.32558238], [372.49090317, 232.1539733, 254.36398731, -141.96114816]]
+test_momenta = [
+    [500., 0., 0., 500.],
+    [500., 0., 0., -500.],
+    [253.58419798, -239.58965912, 66.81985738, -49.36443422],
+    [373.92489886, 7.43568582, -321.18384469, 191.32558238],
+    [372.49090317, 232.1539733, 254.36398731, -141.96114816]
+]
 
-testing_X = NN.process_testing_data(test_momenta)
+testing_X = NN.process_testing_data(moms=test_momenta)
 
 pred_std = model.predict(testing_X)
-pred_destd = NN.destandardise_data(pred_std)
+pred_destd = NN.destandardise_data(y_pred=pred_std)
 
 with open ('./single_test_arch.json', 'w') as fout:
     fout.write(model.to_json())
