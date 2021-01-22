ae5bca030d5d4743f1c73a75bb291a2022c1feb3
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2021-01-22 13:43:14 +0000

    adding run_grid_convert

---
+++ n3jet/runs/runs/diphoton/3g2A/RAMBO/delta_grid/run_grid_convert.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+shopt -s nullglob
+for f in *.yaml
+do
+    python /mt/home/jbullock/n3jet/n3jet/c++_calls/conversion/modeldump_multiple_fks.py --yaml_file $f -p 9 -ob /mt/home/jbullock/n3jet/n3jet/c++_calls/models/diphoton/3g2A/RAMBO/delta_grid/ &
+done
