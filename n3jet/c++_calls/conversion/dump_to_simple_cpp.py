66875d76b0be6ef5df5059d113616b27901a63e6
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2021-03-09 15:49:00 +0000

    read out bug fix single tests running for c++ calls

--- n3jet/c++_calls/conversion/dump_to_simple_cpp.py
+++ n3jet/c++_calls/conversion/dump_to_simple_cpp.py
@@ -49,28 +49,28 @@ with open(args.output, 'w') as fout:
             fout.write(str(l['config']['pool_size'][0]) + ' ' + str(l['config']['pool_size'][1]) + '\n')
         if l['class_name'] == 'Dense':
 
-            # # go through weight layers
-            # W = model.layers[ind].get_weights()[0]
-            # fout.write(str(W.shape[0]) + ' ' + str(W.shape[1]) + '\n')
-            # fout.write('[')
-            # for w in W:
-            #     for i in w:
-            #         fout.write(str(i) + ' ')
-            #     fout.write(']' + '\n')
-                
-            # # go through bias terms
-            # W = model.layers[ind].get_weights()[1]
-            # fout.write('[ ')
-            # for i in W:
-            #     fout.write(str(i) + ' ')
-            # fout.write(']' + '\n')
-
+            # go through weight layers
             W = model.layers[ind].get_weights()[0]
-
-            if args.verbose:
-                print (W.shape)
             fout.write(str(W.shape[0]) + ' ' + str(W.shape[1]) + '\n')
-
             for w in W:
-                fout.write(str(w) + '\n')
-            fout.write(str(model.layers[ind].get_weights()[1]) + '\n')
+                fout.write('[')
+                for i in w:
+                    fout.write(str(i) + ' ')
+                fout.write(']' + '\n')
+                
+            # go through bias terms
+            W = model.layers[ind].get_weights()[1]
+            fout.write('[')
+            for i in W:
+                fout.write(str(i) + ' ')
+            fout.write(']' + '\n')
+
+            # W = model.layers[ind].get_weights()[0]
+
+            # if args.verbose:
+            #     print (W.shape)
+            # fout.write(str(W.shape[0]) + ' ' + str(W.shape[1]) + '\n')
+
+            # for w in W:
+            #     fout.write(str(w) + '\n')
+            # fout.write(str(model.layers[ind].get_weights()[1]) + '\n')
