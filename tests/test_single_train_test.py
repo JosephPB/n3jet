7e899cf5a2eec04972929288227b84bd695279db
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2021-04-01 14:14:47 +0100

    adding recut_data tests to single_train_test

--- tests/test_single_train_test.py
+++ tests/test_single_train_test.py
@@ -18,4 +18,38 @@ def test__yaml_readin():
     assert singlemodel.delta_cut == y["delta_cut"]
     assert singlemodel.delta_near == y["delta_near"]
 
-def test__recut_data()
+def test__recut_data(dummy_data_all_legs_training):
+
+    momenta, cut_mom, near_mom, labels, cut_labs, near_labs, delta_cut, delta_near = dummy_data_all_legs_training
+
+    singlemodel = SingleModelRun.from_yaml(example_config)
+    singlemodel.delta_cut = 0.0
+    singlemodel.delta_near = 0.02
+
+    mom, nj = singlemodel.recut_data(momenta, labels)
+
+    assert len(mom) == len(momenta)
+    assert len(nj) == len(labels)
+
+    singlemodel.delta_cut = 0.1
+    mom, nj = singlemodel.recut_data(momenta, labels)
+
+    assert len(mom) < len(momenta)
+    assert len(nj) < len(labels)
+
+def test__train(dummy_data_all_legs_training):
+    
+    momenta, cut_mom, near_mom, labels, cut_labs, near_labs, delta_cut, delta_near = dummy_data_all_legs_training
+
+    singlemodel = SingleModelRun.from_yaml(example_config)
+    fksmodel.delta_cut = delta_cut
+    fksmodel.delta_near = delta_near
+    fksmodel.model_base_dir = ""
+    fksmodel.model_dir = ""
+    fksmodel.nlegs = 3
+    fksmodel.epochs = 1
+    fksmodel.training_reruns = 1
+    
+    cut_momenta, near_momenta, cut_nj, near_nj, pairs, near_nj_split = fksmodel.split_data(momenta, labels)
+
+    fksmodel.train_networks(cut_momenta, near_momenta, cut_nj, near_nj, pairs, near_nj_split)
